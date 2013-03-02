ecd4b8076a09c51f2029be6867927fbba2f6b063
Megal Williams <megalwilliams@fullsail.edu>
2013-03-02 14:32:46 -0500

    This is the juicy stuff

---
+++ json.js
@@ -0,0 +1,56 @@
+// Week 4, Project 4
+// Megal Williams
+// VFW 1302
+// json.js
+
+var json = {
+		"song1": {
+			"songInfo": ["Select Song Type:", "Hip hop"],
+			"sname": ["Name of Song:", "Stank like Badank"],
+			"aname": ["Album:", "Goodwill"],
+			"arname": ["Artist:", "Boogy B"],
+			"url": ["Music Down website:", "http://www.apple.com"],
+			"favorite": ["Favorite:", "Yes"],
+			"fav": ["Save as Favorite:", "Yes"],
+			"range": ["Rating:", "9"],
+			"date": ["Date:", "2013-3-1"],
+			"notes": ["Notes:", "VFW"]
+		},
+		"song2": {
+			"songInfo": ["Select Song Type:", "R&B"],
+			"sname": ["Name of Song:", "Lusty Love"],
+			"aname": ["Album:", "True Whispers"],
+			"arname": ["Artist:", "Slow Turn"],
+			"url": ["Music Down website:", "http://www.yahoo.com"],
+			"favorite": ["Favorite:", "No"],
+			"fav": ["Save as Favorite:", "Yes"],
+			"range": ["Rating:", "10"],
+			"date": ["Date:", "2013-3-1"],
+			"notes": ["Notes:", "VFW"]
+		},
+		"song3": {
+			"songInfo": ["Select Song Type:", "Reggae"],
+			"sname": ["Name of Song:", "Bun Dem Heart"],
+			"aname": ["Album:", "yu"],
+			"arname": ["Artist:", "Grand Ras"],
+			"url": ["Music Down website:", "http://www.hulkshare.com"],
+			"favorite": ["Favorite:", "Yes"],
+			"fav": ["Save as Favorite:", "Yes"],
+			"range": ["Rating:", "10"],
+			"date": ["Date:", "2013-3-1"],
+			"notes": ["Notes:", "VFW"]
+		},
+		"song4": {
+			"songInfo": ["Select Song Type:", "Other"],
+			"sname": ["Name of Song:", "Jazz Cool"],
+			"aname": ["Album:", "Jazz Time"],
+			"arname": ["Artist:", "Cool Breeze"],
+			"url": ["Music Down website:", "http://www.apple.com"],
+			"favorite": ["Favorite:", "Yes"],
+			"fav": ["Save as Favorite:", "Yes"],
+			"range": ["Rating:", "10"],
+			"date": ["Date:", "2013-02-28"],
+			"notes": ["Notes:", "VFW"]
+		}
+
+	}
